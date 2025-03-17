6fabdca7fb60c95ac2aa4a9d0a66c58be5447179
AbdullahAlimam <helix.abdu@gmail.com>
2025-03-17 16:26:53 +0100

    Enhance XML documentation and update DomainValidator to validate both HostValidator and TldValidator.

---
+++ Helpers/DomainValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace CORSProxy.Helpers
+{
+    /// <summary>
+    /// Provides validation methods for domains, including checking for valid IP addresses and TLDs.
+    /// </summary>
+    public static class DomainValidator
+    {
+        /// <summary>
+        /// Validates whether the given input is a valid domain.
+        /// A valid domain can be either an IP address (IPv4/IPv6) or a domain with a valid TLD.
+        /// </summary>
+        /// <param name="input">The domain or IP address to validate.</param>
+        /// <returns><c>true</c> if the input is a valid domain or IP address; otherwise, <c>false</c>.</returns>
+        public static bool IsValidDomain(string input)
+        {
+            // Check if the input is a valid IP (IPv4/IPv6)
+            if (HostValidator.IsValidHost(input))
+            {
+                return true;
+            }
+
+            // Check if the input has a valid Top-Level Domain (TLD)
+            return TldValidator.IsValidTld(input);
+        }
+    }
+}
+
